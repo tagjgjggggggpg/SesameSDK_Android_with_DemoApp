5844ad2c6a5738e72e59c81404f9b44cdf9d7c02
tagjgjggggggpg <taiyaki.09180107@gmail.com>
2026-08-27 17:47:47 +0900

    Add secure entrance Quick Settings tile

---
+++ app/src/main/java/co/candyhouse/app/lockgroup/EntranceQuickSettingsTileService.kt
@@ -0,0 +1,51 @@
+package co.candyhouse.app.lockgroup
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+
+class EntranceQuickSettingsTileService : TileService() {
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile?.apply {
+            label = "玄関"
+            state = Tile.STATE_INACTIVE
+            updateTile()
+        }
+    }
+
+    override fun onClick() {
+        super.onClick()
+        val launch = Runnable { launchGroupActivity() }
+        if (isSecure && isLocked) {
+            unlockAndRun(launch)
+        } else {
+            launch.run()
+        }
+    }
+
+    private fun launchGroupActivity() {
+        val intent = Intent(this, LockGroupActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val pendingIntent = PendingIntent.getActivity(
+                this,
+                REQUEST_CODE,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+            )
+            startActivityAndCollapse(pendingIntent)
+        } else {
+            @Suppress("DEPRECATION")
+            startActivityAndCollapse(intent)
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE = 41001
+    }
+}
